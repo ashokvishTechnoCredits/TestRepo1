60d7c60e5ef5776258d13c59bf9df1df9d7e9f54
Ashok Vishwakarma <ashokcogni@gmail.com>
2022-01-23 21:31:11 +0530

    B1

--- src/P1/Test_p1.java
+++ src/P1/Test_p1.java
@@ -3,7 +3,7 @@ package P1;
 public class Test_p1 {
 	
 	static void m2() {
-		System.out.println("This is TEst p1");
+		System.out.println("This is TEst p1, adddingg gg g ");
 	}
 
 	public static void main(String[] args) {
